7bb1d4ad544b75d216b526212f9679186b8fde3d
Mohamed HAKI <Mohamedhaki70@gmail.com>
2023-10-20 16:48:59 +0100

    add the fonction of calcule

---
+++ index.js
@@ -0,0 +1,50 @@
+function calcule(){
+    switch(operation){
+        case '+':
+            var table = inputt.innerText.split('+');
+            var sum =0;
+            for(let i =0 ; i <table.length;i++){
+                sum += Number(table[i]);
+            }
+            inputt.innerHTML= sum;
+            break;
+
+    
+    case '-':
+        var table = inputt.innerText.split('-');
+        var diff =table[0];
+        for(let i =1 ; i <table.length;i++){
+            diff -= Number(table[i]) ;
+        }
+        inputt.innerHTML= diff;
+    break;
+
+    case '*':
+        var table = inputt.innerText.split('*');
+        var mult =table[0];
+        for(let i =1 ; i <table.length;i++){
+            mult *= Number(table[i]);
+        }
+        inputt.innerHTML= mult;
+        break;
+
+        case '/':
+            var table = inputt.innerText.split('/');
+            var diff =table[0];
+            for(let i =1 ; i <table.length;i++){
+                if(table[i]>0){
+                    diff /= Number(table[i]);
+                    inputt.innerHTML= diff;
+                }
+                else 
+                inputt.innerHTML='sorry';
+              
+                
+            }
+            
+         break;
+
+
+
+}
+}
